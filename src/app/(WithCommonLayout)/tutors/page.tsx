2b321fedd9337af5082d4768599921e2c52c1547
tafiya <tafiapinkey@gmail.com>
2025-03-11 16:21:45 +0600

    fix some build issue

--- src/app/(WithCommonLayout)/tutors/page.tsx
+++ src/app/(WithCommonLayout)/tutors/page.tsx
@@ -1,19 +1,19 @@
-import FindTutors from '@/components/tutor/Turors';
-import { getAllTutors } from '@/services/Tutor';
-import React from 'react';
+import FindTutors from "@/components/tutor/Turors";
+import { getAllTutors } from "@/services/Tutor";
+import React, { Suspense } from "react";
 
-const TutorsPage =async () => {
-    const data= await getAllTutors();
+const TutorsPage = async () => {
+  const data = await getAllTutors();
 
-    const tutors = data?.data;
-    return (
-        <div>
-    <div className="w-full flex justify-center items-center bg-[#eff6ff] h-64 md:h-80 ">
+  const tutors = data?.data;
+  return (
+    <div>
+      <div className="w-full flex justify-center items-center bg-[#eff6ff] h-64 md:h-80 ">
         <div className=" mt-16">
           <h2 className=" text-xl sm:text-2xl md:text-3xl xl:text-4xl font-semibold leading-snug text-balance text-center">
             Find your <span className=" text-blue-600 ">Perfect </span>
             <br />
-          Tutor
+            Tutor
           </h2>
           <h2 className=" text-center text-blue-600 text-lg font-medium mt-3">
             {" "}
@@ -21,9 +21,11 @@ const TutorsPage =async () => {
           </h2>
         </div>
       </div>
-    <FindTutors tutors={tutors}></FindTutors>
-        </div>
-    );
+      <Suspense fallback={<>loading..</>}>
+        <FindTutors tutors={tutors}></FindTutors>
+      </Suspense>
+    </div>
+  );
 };
 
 export default TutorsPage;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,12 @@
 import LoginFrom from "@/components/auth/LoginFrom";
+import { Suspense } from "react";
 
 const page = () => {
   return (
     <div className=" min-h-screen flex items-center justify-center">
-<LoginFrom></LoginFrom>
+      <Suspense fallback={<>loading...</>}>
+        <LoginFrom></LoginFrom>
+      </Suspense>
     </div>
   );
 };
